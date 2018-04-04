f033182712cdbd0c404f830cf7af390febd72651
Stacie Taylor-Cima <stacietaylorcima@Stacies-MacBook-Air.local>
2018-04-04 13:20:50 -0700

    implement privacy controls and downgrade private wikis

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,19 @@
 class UsersController < ApplicationController
+
   def show
     @wikis = Wiki.where(user_id: current_user.id)
   end
 
   def downgrade
     @user = current_user
-    current_user.update_attribute(:role, 'standard')
+    wikis = Wiki.all
 
+    current_user.update_attribute(:role, 'standard')
+    wikis.each do |wiki|
+      if wiki.user.id == current_user.id
+        wiki.update_attribute(:private, false)
+      end
+    end 
     flash[:notice] = "Your account has been downgraded. You are now a Standard member."
     redirect_to users_show_path
   end
