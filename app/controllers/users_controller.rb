47a0d4b2865f2762c1d5c8d3e46d81d0f22b338f
Stacie Taylor-Cima <stacietaylorcima@Stacies-MacBook-Air.local>
2018-04-06 20:29:29 -0700

    refactor downgrade method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,9 @@ class UsersController < ApplicationController
     @user = current_user
     wikis = Wiki.all
 
-    current_user.update_attribute(:role, 'standard')
-    wikis.each do |wiki|
-      if wiki.user.id == current_user.id
-        wiki.update_attribute(:private, false)
-      end
-    end 
+    current_user.standard!
+    current_user.wikis.update_all(private: false)
+    
     flash[:notice] = "Your account has been downgraded. You are now a Standard member."
     redirect_to users_show_path
   end
